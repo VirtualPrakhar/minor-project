a12c8eb101ed03cd87ea13216c7c26ba572e21c7
Prakhar Verma <156530259+VirtualPrakhar@users.noreply.github.com>
2024-11-19 13:33:23 +0000

    home page commited

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -7,7 +7,32 @@ function Home() {
             <div className="col-12 col-md-6">
                 <div className="card shadow-sm p-2 mb-5 bg-secondry rounded">
                     <div className="card-body text-center bg-dark">
-                        <img src="/images/codecast.png" alt="CodeCast"></img>
+                        <img 
+                            className="img-fluid mx-auto d-block" 
+                            src="/images/codecast.png" 
+                            alt="CodeCast" 
+                            style={{maxWidth:"150px" }}
+                        />
+                        <h4 className="text-light">Enter Room ID</h4>
+                        <div className="form-group">
+                            <input 
+                                type="text" 
+                                className="form-control mb-2" 
+                                placeholder="ROOM ID" 
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input 
+                                type="text" 
+                                className="form-control mb-2" 
+                                placeholder="Username" 
+                            />
+                        </div>
+                        <button className="btn btn-success btn-lg btn-block">JOIN</button>
+                        <p className="mt-3 text-light">
+                            Don't have a Room ID?{" "} 
+                            <span className="text-success p-2" style={{cursor:"pointer"}}>Create New Room</span>
+                        </p>
                     </div>
                 </div>
             </div>
