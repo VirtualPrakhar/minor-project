7abf586ec993c840e7095a68e7d9dac5ab4110a7
Prakhar Verma <156530259+VirtualPrakhar@users.noreply.github.com>
2024-11-19 13:17:35 +0000

    addded

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-function Home(){
-    return(
-        <div>Home Page</div>
-    )
+function Home() {
+  return (
+    <div className="container-fluid">
+        <div className="row justify-content-center align-items-center min-vh-100">
+            <div className="col-12 col-md-6">
+                <div className="card shadow-sm p-2 mb-5 bg-secondry rounded">
+                    <div className="card-body text-center bg-dark">
+                        <img src="/images/codecast.png" alt="CodeCast"></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
 }
 
 export default Home;
