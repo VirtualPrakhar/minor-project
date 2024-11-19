2f13dbb5427012890ad402109abdc0734dca8065
Prakhar Verma <156530259+VirtualPrakhar@users.noreply.github.com>
2024-11-19 16:22:13 +0000

    editor page initial commits

--- client/src/component/EditorPage.js
+++ client/src/component/EditorPage.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
+import Client from "./Client";
 
 function EditorPage() {
+  const [clients, setClients] = useState([
+    {socketId: 1, username: "Pratyush"},
+    {socketId: 2, username: "Pooja"},
+  ])
   return (
-    <div>Editor Page</div>
+    <div className="container-fluid vh-100">
+      <div className="row h-100">
+        <div className="col-md-2 bg-dark text-light d-flex flex-column h-100" style={{boxShadow:"2px 0px 4px rgba(0,0,0,0.1)" }}>
+          <img 
+            src="/images/codecast.png" 
+            alt="CodeCast" 
+            className="img-fluid mx-auto"
+            style={{maxWidth:'150px', marginTop:"-43px"}}
+          />
+          <hr style={{marginTop:"-3rem"}} />
+          {/* Client List Container */}
+          <div className="d-flex flex-column overflow-auto">
+            {clients.map((client) => {
+              <Client key={client.socketId} username={client.username}/>
+            })}
+          </div>
+            {/* Buttons */}
+            <div className="mt-auto">
+              <hr />
+              <button className="btn btn-success">Copy ROOM ID</button>
+              <button className="btn btn-danger mt-2 mb-2 px-3 btn-block">
+                Leave ROOM
+              </button>
+            </div>
+        </div>
+        {/* EDITOR */}
+        <div className="col-md-10 text-light d-flex flex-column h-100">
+          Editor
+        </div>
+      </div>
+    </div>
   )
 }
 
